b35fda523788797aa4961ff031603233c1440e25
davilaICOP <130504892+davilaICOP@users.noreply.github.com>
2023-06-06 13:25:07 -0300

    Add files via upload

--- Práctica - Introducción a objetos/Ejercicio N°2/Entidad/Circunferencia.java
+++ Práctica - Introducción a objetos/Ejercicio N°2/Entidad/Circunferencia.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 
 public class Circunferencia {
     private double radio;
-    private double area;
-    private double perimetro;
+    double area;
+    double perimetro;
 
     // Método constructor que inicializa el radio pasado como parámetro
     public Circunferencia(double radio) {
-        this.radio = radio;
+        super();
+    	this.radio = radio;
     }
 
     // Método getter para el atributo radio
@@ -30,20 +31,14 @@ public class Circunferencia {
 
     // Método para calcular el área de la circunferencia
     public void calcularArea() {
-        area = Math.PI * Math.pow(radio, 2);
+    	area = Math.PI * Math.pow(radio, 2);
+    	System.out.println("El área es: "+area);
     }
 
     // Método para calcular el perímetro de la circunferencia
     public void calcularPerimetro() {
         perimetro = 2 * Math.PI * radio;
+        System.out.println("El perimetro es: "+perimetro);
     }
     
-    // Métodos getter para los atributos area y perimetro
-    public double getArea() {
-        return area;
-    }
-
-    public double getPerimetro() {
-        return perimetro;
-    }
 }
